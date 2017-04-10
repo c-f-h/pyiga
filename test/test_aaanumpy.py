99abd364d583c37e0aba06676365c213d16db96b
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2017-04-10 13:13:36 +0200

    oops

--- test/test_aaanumpy.py
+++ test/test_aaanumpy.py
@@ -2,4 +2,4 @@ def test_bspline_cy():
     from pyiga import bspline, geometry, assemble_tools_cy
     kv = bspline.make_knots(3, 0.0, 1.0, 10)
     geo = geometry.bspline_quarter_annulus()
-    asm = assemble_tools_cy.StiffnessAssembler2D((kv,kv) geo=geo)
+    asm = assemble_tools_cy.StiffnessAssembler2D((kv,kv), geo=geo)

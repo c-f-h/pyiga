c33ecdbf1cc4468c99bcc48738662a43c81d8a8d
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2017-05-08 20:47:46 +0200

    install future only on Py2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     install_requires = [
         'numpy',
         'scipy',
-        'future',
+        'future;python_version<"3.0"',
         'futures;python_version<"3.0"'   # backport of concurrent.futures to Py2
     ],
 

5d35832e414e6b0b2832d4913ee5b23348271ff1
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2017-06-02 14:06:27 +0200

    allow functions returning tuples in grid_eval

--- pyiga/utils.py
+++ pyiga/utils.py
@@ -2,6 +2,14 @@ import numpy as np
 import scipy.sparse
 import scipy.sparse.linalg
 
+def _broadcast_to_grid(X, grid_shape):
+    num_dims = len(grid_shape)
+    # if the field X is not scalar, we need include the extra dimensions
+    target_shape = grid_shape + X.shape[num_dims:]
+    if X.shape != target_shape:
+        X = np.broadcast_to(X, target_shape)
+    return X
+
 def grid_eval(f, grid):
     if hasattr(f, 'grid_eval'):
         return f.grid_eval(grid)
@@ -10,14 +18,17 @@ def grid_eval(f, grid):
         mesh.reverse() # convert order ZYX into XYZ
         values = f(*mesh)
 
+        grid_shape = tuple(len(g) for g in grid)
+
+        # if the function returned a tuple, interpret as vector-valued function
+        if isinstance(values, tuple):
+            values = np.stack(tuple(_broadcast_to_grid(v, grid_shape) for v in values),
+                    axis=-1)
+
         # If f is a function which uses only some of its arguments,
         # values may have the wrong shape. In that case, broadcast
         # it to the full mesh extent.
-        num_dims = len(grid)
-        # if the function is not scalar, we need include the extra dimensions
-        target_shape = tuple(len(g) for g in grid) + values.shape[num_dims:]
-        if values.shape != target_shape:
-            values = np.broadcast_to(values, target_shape)
+        values = _broadcast_to_grid(values, grid_shape)
 
         return values
 

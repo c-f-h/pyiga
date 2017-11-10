cc66b2ced241b6c952b789abc7a59bfd961381d9
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2017-11-10 11:33:40 +0100

    geo is now optional in plot_field

--- pyiga/vis.py
+++ pyiga/vis.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 
 from . import utils
 
-def plot_field(field, geo, res=80, **kwargs):
-    grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in geo.kvs)
-    XY = utils.grid_eval(geo, grd)
-    C = utils.grid_eval(field, grd)
+def plot_field(field, geo=None, res=80, **kwargs):
     kwargs.setdefault('shading', 'gouraud')
-    return plt.pcolormesh(XY[...,0], XY[...,1], C, **kwargs)
+    if geo is not None:
+        grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in geo.kvs)
+        XY = utils.grid_eval(geo, grd)
+        C = utils.grid_eval(field, grd)
+        return plt.pcolormesh(XY[...,0], XY[...,1], C, **kwargs)
+    else:
+        # assumes that `field` is a BSplineFunc
+        grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in field.kvs)
+        C = utils.grid_eval(field, grd)
+        return plt.pcolormesh(grd[1], grd[0], C, **kwargs)

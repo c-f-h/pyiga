001855dc7b2a3b9cd94a659e7ff3009af6d8360f
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2018-01-09 13:59:35 +0100

    add `plot_geo` function to vis module

--- pyiga/vis.py
+++ pyiga/vis.py
@@ -16,3 +16,34 @@ def plot_field(field, geo=None, res=80, **kwargs):
         grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in field.kvs)
         C = utils.grid_eval(field, grd)
         return plt.pcolormesh(grd[1], grd[0], C, **kwargs)
+
+
+def plot_geo(geo,
+             grid=10, gridx=None, gridy=None,
+             res=50,
+             linewidth=None, color='black'):
+    """Plot a wireframe representation of a 2D geometry."""
+    assert geo.dim == geo.sdim == 2, 'Can only plot 2D geometries'
+    if gridx is None: gridx = grid
+    if gridy is None: gridy = grid
+    supp = geo.support
+    gx    = np.linspace(supp[0][0], supp[0][1], gridx)
+    meshx = np.linspace(supp[0][0], supp[0][1], res)
+    gy    = np.linspace(supp[1][0], supp[1][1], gridy)
+    meshy = np.linspace(supp[1][0], supp[1][1], res)
+
+    def plotline(pts, capstyle='butt'):
+        plt.plot(pts[:,0], pts[:,1], color=color, linewidth=linewidth,
+                solid_joinstyle='round', solid_capstyle=capstyle)
+
+    pts = utils.grid_eval(geo, (gx, meshy))
+    plotline(pts[0,:,:], capstyle='round')
+    for i in range(1, pts.shape[0]-1):
+        plotline(pts[i,:,:])
+    plotline(pts[-1,:,:], capstyle='round')
+
+    pts = utils.grid_eval(geo, (meshx, gy))
+    plotline(pts[:,0,:], capstyle='round')
+    for j in range(1, pts.shape[1]-1):
+        plotline(pts[:,j,:])
+    plotline(pts[:,-1,:], capstyle='round')

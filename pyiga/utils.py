a7bf6b87a9b8752493aecf4dc7dd421945d411c3
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2018-01-12 12:49:02 +0100

    make sure grid_eval_transformed returns the proper shape of array

--- pyiga/utils.py
+++ pyiga/utils.py
@@ -12,6 +12,21 @@ def _broadcast_to_grid(X, grid_shape):
         X = np.broadcast_to(X, target_shape)
     return X
 
+def _ensure_grid_shape(values, grid):
+    """Convert tuples of arrays into higher-dimensional arrays and make sure
+    the array conforms to the grid size."""
+    grid_shape = tuple(len(g) for g in grid)
+
+    # if values is a tuple, interpret as vector-valued function
+    if isinstance(values, tuple):
+        values = np.stack(tuple(_broadcast_to_grid(v, grid_shape) for v in values),
+                axis=-1)
+
+    # If values came from a function which uses only some of its arguments,
+    # values may have the wrong shape. In that case, broadcast it to the full
+    # mesh extent.
+    return _broadcast_to_grid(values, grid_shape)
+
 def grid_eval(f, grid):
     if hasattr(f, 'grid_eval'):
         return f.grid_eval(grid)
@@ -19,20 +34,7 @@ def grid_eval(f, grid):
         mesh = np.meshgrid(*grid, sparse=True, indexing='ij')
         mesh.reverse() # convert order ZYX into XYZ
         values = f(*mesh)
-
-        grid_shape = tuple(len(g) for g in grid)
-
-        # if the function returned a tuple, interpret as vector-valued function
-        if isinstance(values, tuple):
-            values = np.stack(tuple(_broadcast_to_grid(v, grid_shape) for v in values),
-                    axis=-1)
-
-        # If f is a function which uses only some of its arguments,
-        # values may have the wrong shape. In that case, broadcast
-        # it to the full mesh extent.
-        values = _broadcast_to_grid(values, grid_shape)
-
-        return values
+        return _ensure_grid_shape(values, grid)
 
 def grid_eval_transformed(f, grid, geo):
     """Transform the tensor grid `grid` by the geometry transform `geo` and
@@ -43,9 +45,7 @@ def grid_eval_transformed(f, grid, geo):
     X = tuple(trf_grid[..., i] for i in range(trf_grid.shape[-1]))
     # evaluate the function
     vals = f(*X)
-    if isinstance(vals, tuple):
-        vals = np.stack(vals, axis=-1)
-    return vals
+    return _ensure_grid_shape(vals, grid)
 
 def read_sparse_matrix(fname):
     I,J,vals = np.loadtxt(fname, skiprows=1, unpack=True)

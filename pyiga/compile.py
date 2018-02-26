b06388e63e77d7a2d400700542b035880970350b
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2018-02-26 13:22:02 +0100

    add some default compile args for faster code

--- pyiga/compile.py
+++ pyiga/compile.py
@@ -30,7 +30,7 @@ def _compile_cython_module_nocache(src, modname, verbose=False):
     include_dirs = [
         numpy.get_include()
     ]
-    extra_compile_args = []
+    extra_compile_args = ['-O3', '-march=native', '-ffast-math', '-fopenmp']
 
     extension = Extension(name=modname,
                           language="c++",

956839304e28758a96dd03f4857f264639535a69
Clemens Hofreither <chofreither@numa.uni-linz.ac.at>
2018-03-04 17:48:55 +0100

    add animate_field function

--- pyiga/vis.py
+++ pyiga/vis.py
@@ -1,19 +1,23 @@
 """Visualization functions."""
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 from . import utils
 
 def plot_field(field, geo=None, res=80, **kwargs):
+    """Plot a scalar field, optionally over a geometry."""
     kwargs.setdefault('shading', 'gouraud')
+    if np.isscalar(res):
+        res = (res, res)
     if geo is not None:
-        grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in geo.kvs)
+        grd = tuple(np.linspace(s[0], s[1], r) for (s,r) in zip(geo.support, res))
         XY = utils.grid_eval(geo, grd)
         C = utils.grid_eval(field, grd)
         return plt.pcolormesh(XY[...,0], XY[...,1], C, **kwargs)
     else:
-        # assumes that `field` is a BSplineFunc
-        grd = tuple(np.linspace(kv.support()[0], kv.support()[1], res) for kv in field.kvs)
+        # assumes that `field` is a BSplineFunc or equivalent
+        grd = tuple(np.linspace(s[0], s[1], r) for (s,r) in zip(field.support, res))
         C = utils.grid_eval(field, grd)
         return plt.pcolormesh(grd[1], grd[0], C, **kwargs)
 
@@ -58,3 +62,31 @@ def plot_curve(geo, res=50, linewidth=None, color='black'):
     mesh = np.linspace(supp[0][0], supp[0][1], res)
     pts = utils.grid_eval(geo, (mesh,))
     plt.plot(pts[:,0], pts[:,1], color=color, linewidth=linewidth)
+
+
+def animate_field(fields, geo, vrange=None, res=(50,50), cmap=None, interval=50):
+    """Animate a sequence of scalar fields over a geometry."""
+    fields = list(fields)
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    if np.isscalar(res):
+        res = (res, res)
+    grd = tuple(np.linspace(s[0], s[1], r) for (s,r) in zip(geo.support, res))
+    XY = geo.grid_eval(grd)
+    C = np.zeros(res)
+
+    if vrange is None:
+        # determine range of values from first field
+        C = utils.grid_eval(fields[0], grd)
+        vrange = (C.min(), C.max())
+
+    quadmesh = plt.pcolormesh(XY[...,0], XY[...,1], C, shading='gouraud', cmap=cmap,
+                vmin=vrange[0], vmax=vrange[1], axes=ax)
+    fig.colorbar(quadmesh, ax=ax)
+
+    def anim_func(i):
+        C = utils.grid_eval(fields[i], grd)
+        quadmesh.set_array(C.ravel())
+
+    return animation.FuncAnimation(fig, anim_func, frames=len(fields), interval=interval)

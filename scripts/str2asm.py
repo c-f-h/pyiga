af24d61b28bf71cd2d8b0230f05f3e499a772c0d
Clemens Hofreither <chofreither@gmail.com>
2020-01-23 11:00:12 +0100

    some extensions to str2asm tool

--- scripts/str2asm.py
+++ scripts/str2asm.py
@@ -8,9 +8,11 @@ def parse_args():
     parser.add_argument('expr', type=str, help='the expression to be compiled')
     parser.add_argument('--dim', type=int, default=2, help='the space dimension')
     parser.add_argument('--name', type=str, default='CustomAsm', help='the name of the assembler class')
+    parser.add_argument('-o', '--output', help='the file to write to; by default, write to stdout')
     parser.add_argument('--scalarinput', nargs='*', metavar='NAME', help='names of scalar input fields')
     parser.add_argument('--vectorinput', nargs='*', metavar='NAME', help='names of vector input fields')
     parser.add_argument('--ondemand', action='store_true', help='create an on demand assembler')
+    parser.add_argument('--dumptree', action='store_true', help='write the expression tree to stdout')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -29,10 +31,16 @@ if __name__ == '__main__':
         for funcname in _args.vectorinput:
             locals()[funcname] = vf.input(funcname, shape=(_args.dim,))
 
-    vf.add(eval(_args.expr, vars(vform), locals()))
+    e = eval(_args.expr, vars(vform), locals())
+    if _args.dumptree:
+        vform.tree_print(e)
+    vf.add(e)
 
     code = backend.CodeGen()
     backend.AsmGenerator(vf, _args.name, code, on_demand=_args.ondemand).generate()
 
-    print(backend.preamble())
-    print(code.result())
+    f = open(_args.output, 'w') if _args.output else None
+    print(backend.preamble(), file=f)
+    print(code.result(), file=f)
+    if _args.output:
+        f.close()

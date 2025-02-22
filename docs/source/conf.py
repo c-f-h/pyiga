5d1b2b655e1e524f05ff0274d57c59e000c3647c
Clemens Hofreither <chofreither@gmail.com>
2025-02-22 09:57:45 +0200

    add readthedocs configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyiga'
-copyright = '2017-2021, Clemens Hofreither'
+copyright = '2017-2025, Clemens Hofreither'
 author = 'Clemens Hofreither'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -76,7 +76,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

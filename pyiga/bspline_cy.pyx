169ae9d993093bc5ca6ae0fe06cfbf80b1f5733e
Stefan714 <stefan.tyoler@hotmail.de>
2025-07-27 23:23:42 +0200

    update

--- pyiga/bspline_cy.pyx
+++ pyiga/bspline_cy.pyx
@@ -448,7 +448,7 @@ cpdef int pyx_findspan(double[::1] kv, int p, double u) noexcept nogil:
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef object pyx_findspans(double[::1] kv, int p, double[::1] u):
-    out = np.empty(u.shape[0], dtype=np.long)
+    out = np.empty(u.shape[0], dtype=np.int64)
     cdef long[::1] result = out
     cdef int i
     for i in range(u.shape[0]):
